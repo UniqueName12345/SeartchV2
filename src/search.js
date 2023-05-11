093da203968fb3a3c2fd73731a5328f275fef045
AAAAAAAAAA <88826851+UniqueName12345@users.noreply.github.com>
2023-05-11 13:27:31 +0800

    significantly improve code

--- src/search.js
+++ src/search.js
@@ -1,10 +1,11 @@
 function projects(thing) {
-    window.open("https://scratch.mit.edu/search/projects?q=" + thing);
-  }
-
-function studios(thing) {
-    window.open("https://scratch.mit.edu/search/studios?q=" + thing);
+    window.open(`https://scratch.mit.edu/search/projects?q=${thing}`);
 }
+function studios(thing) {
+    window.open(`https://scratch.mit.edu/search/studios?q=${thing}`);
+  }
+  
 function forums(thing) {
-    window.open("https://ocular.jeffalo.net/search?q=" + thing);
+    alert("Temporarily out of order while coding in a way to check if ScratchDB is up before searching.")
+    // window.open("https://ocular.jeffalo.net/search?q=" + thing);
 }
