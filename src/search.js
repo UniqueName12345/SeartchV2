810a0a2e617758955b5f00db4c8fb73c153e0775
AAAAAAAAAA <88826851+UniqueName12345@users.noreply.github.com>
2023-10-06 16:38:01 +0800

    Added some features from the broken branch

--- src/search.js
+++ src/search.js
@@ -1,21 +1,45 @@
 function projects(thing) {
-    window.open(`https://scratch.mit.edu/search/projects?q=${thing}`);
+  const encodedThing = encodeURIComponent(thing);
+  const searchUrl = `https://scratch.mit.edu/search/projects?q=${encodedThing}`;
+  console.log(`Search URL: ${searchUrl}`);
+  window.open(searchUrl);
 }
 function studios(thing) {
-    window.open(`https://scratch.mit.edu/search/studios?q=${thing}`);
+    const encodedThing = encodeURIComponent(thing);
+    const url = `https://scratch.mit.edu/search/studios?q=${encodedThing}`;
+    console.log(`Search URL: ${url}`);
+    window.open(url);
 }
 
+/**
+ * Checks if the ScratchDB service is up and running.
+ *
+ * @returns {boolean} Returns true if the ScratchDB service is up, otherwise false.
+ */
 async function isScratchdbUp() {
-    const response = await fetch("https://scratchdb.lefty.one/v3/docs/", { timeout: 5000 });
-    return response.ok;
+    try {
+        console.log("Checking ScratchDB service...");
+        const response = await fetch("https://scratchdb.lefty.one/v3/docs/");
+        console.log("Response:", response);
+        return response.ok;
+    } catch (error) {
+        console.error("Error:", error);
+        return false;
+    }
 }
-
 
 function forums(thing) {
     if (isScratchdbUp()) {
-      window.open(`https://ocular.jeffalo.net/search?q=${thing}`);
+        console.log(`Opening ocular search for ${thing}`);
+        window.open(`https://ocular.jeffalo.net/search?q=${thing}`);
     } else {
-      alert("ERROR: Can't use ocular");
+        console.log("ERROR: Can't use ocular");
+        alert("ERROR: Can't use ocular");
     }
-  }
-  
+}
+
+function wiki(thing) {
+  console.log(`Searching for ${thing} on the Scratch Wiki...`);
+  window.open(`https://en.scratch-wiki.info/w/index.php?search=${thing}`);
+  console.log(`Search for ${thing} complete.`);
+}
